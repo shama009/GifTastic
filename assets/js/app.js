eb6c15338b6ed58cada2e42c3e78aa05c88c4323
Shama <shamaanjum09@outlook.com>
2017-12-07 18:31:20 -0800

    bug fixes: hadling  duplicate buttons and empty input

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,18 @@
 
 $(document).ready(function () {
 
-    //HmC6jYxAxwhp61bde8FJ2MDSN2VdTQ0h
     // my API key - XW2Cj4MK2064FaM505lDN0FeIqr4b5tJ
     var apiKey = 'XW2Cj4MK2064FaM505lDN0FeIqr4b5tJ';
     var searches = [];
+    
 
-    // finds giphy results based on text
+    // finds giphy results based on text    
     // from the user input 
     function getGiphies(userInput) {
 
         // if I do not have data-giphy use userInput
         var input = $(this).attr("data-giphy") || userInput;
+        
 
         // clear out any previous images on the page when there is a new search happening
         $("#content").empty();
@@ -23,21 +24,21 @@ $(document).ready(function () {
             url: queryURL,
             method: 'GET'
         }).done(function (response) {
-            console.log("response: " + response);
             var data = response.data;
+            console.log(data);
             for (var i = 0; i < data.length; i++) {
+
                 // display a list of gifs and ratings
                 var gifDiv = $("<div class='gif-div'>");
                 var newImg = $("<img>");
-                var url = data[i].images.downsized.url;
-                var splitUrl = url.split(".gif");
-                var stillUrl = splitUrl[0] + "_s.gif";
-                console.log(stillUrl);
+                var stillUrl = data[i].images.original_still.url;
+                var origUrl = data[i].images.original.url;
+                
                 // include all required attributes to animate and pause images
                 newImg.attr("src", stillUrl);
                 newImg.addClass("giphy-img");
                 newImg.attr("data-still", stillUrl);
-                newImg.attr("data-animate", url);
+                newImg.attr("data-animate", origUrl);
                 newImg.attr("data-state", "still");
 
                 var rating = data[i].rating;
@@ -51,7 +52,6 @@ $(document).ready(function () {
 
     // when the user finishes searching
     // make a new button for them to click on later
-
     function makeUserSearchButtons(input) {
         var newBtn = $("<button>");
         newBtn.addClass("giphy-btn");
@@ -66,14 +66,28 @@ $(document).ready(function () {
         // Using a submit button instead of a regular button allows the user to hit
         // "Enter" instead of clicking the button if desired
         e.preventDefault();
+
         // take the value from the user-input field
-        var userInput = $("#user-input").val();
-        makeUserSearchButtons(userInput);
+        // for empty input field default: minions
+        var userInput = $("#user-input").val() || "minions";
+
+        // array for user input to check for duplicate buttons
+        // iterate array to make buttons
+        //empty button container and load all buttons 
+        $('.button-container').empty();
+       if(!searches.includes(userInput)){ // checking if user input is already in array
+        searches.push(userInput);
+        }
+        for(var i=0; i<searches.length; i++){
+            makeUserSearchButtons(searches[i]);
+        }
         getGiphies(userInput);
     });
 
     // when the user dynamically generated giphy button
     $(document).on("click", ".giphy-btn", getGiphies);
+
+    // still and animation effects on click of gifs
     $(document).on("click", ".giphy-img", function () {
         var state = $(this).attr("data-state");
         if (state === "still") {
